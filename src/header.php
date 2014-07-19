b2f270a733bca64ef4f9e3bf841c41cc443284ad
Harrison Unruh <htrain9000@gmail.com>
2014-07-19 01:16:21 -0600

    Fixed missing files in last commit

---
+++ src/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title(); ?></title>
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400'  type='text/css'>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php wp_head(); ?>
+    </head>
+
+    <div id="body-wrapper">
+
+    <nav class="navbar-fixed-top">
+        <nav class="navbar navbar-redrock" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">
+                        <img style="float: left;" class="desktop-logo" src="<?php bloginfo('template_directory');?>/images/header/logo-large.png">
+                        <img class="mobile-logo" src="<?php bloginfo('template_directory');?>/images/header/logo-small.png">
+                        <p class="mobile-logo">Red Rock Robotics</p>
+                    </a>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-mobile">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbar-mobile">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Our Team</a></li>
+                        <li><a href="#">Resources</a></li>
+                        <li><a href="#">Calendar</a></li>
+                        <li><a href="#">Sponsors</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </nav>
