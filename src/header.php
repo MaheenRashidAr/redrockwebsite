edb106287ee0ea5b79d03e62c8bb3992bcd47fe8
Harrison Unruh <htrain9000@gmail.com>
2014-07-20 00:10:38 -0600

    Changed license, fixed header bugs and added WordPress functionality, added favicon

--- src/header.php
+++ src/header.php
@@ -2,11 +2,12 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(); ?></title>
+        <title>Red Rock Robotics<?php wp_title(' | '); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400'  type='text/css'>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+        <link rel="icon" type="image/ico" href="images/icon/favicon.ico">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <?php wp_head(); ?>
     </head>
@@ -17,7 +18,7 @@
         <nav class="navbar navbar-redrock" role="navigation">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="<?php get_home_url(); ?>">
                         <img style="float: left;" class="desktop-logo" src="<?php bloginfo('template_directory');?>/images/header/logo-large.png">
                         <img class="mobile-logo" src="<?php bloginfo('template_directory');?>/images/header/logo-small.png">
                         <p class="mobile-logo">Red Rock Robotics</p>
@@ -32,12 +33,27 @@
 
                 <div class="collapse navbar-collapse" id="navbar-mobile">
                     <ul class="nav navbar-nav">
+<!--
                         <li class="active"><a href="#">Home</a></li>
                         <li><a href="#">Our Team</a></li>
                         <li><a href="#">Resources</a></li>
                         <li><a href="#">Calendar</a></li>
                         <li><a href="#">Sponsors</a></li>
                         <li><a href="#">Contact Us</a></li>
+-->
+                        <?php
+                            $pages = get_pages(array('sort_column' => 'ID'));
+                            foreach($pages as $page) {
+                        ?>
+                                <li class="<?php if(is_page($page->ID)) {
+                                    echo "active";
+                                }?>">
+                                    <a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a>
+                                </li>
+                        <?php
+                            }
+                        ?>
+
                     </ul>
                 </div>
             </div>
